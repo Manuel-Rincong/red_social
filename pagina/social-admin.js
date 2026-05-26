19591d03598acea843897cdbf0c832bf93711a31
StarKing1730 <jhoann-hernandezc@unilibre.edu.co>
2026-05-25 20:34:44 -0500

    red social / red social admin funcionando
    todo correcto, me faltan los likes y comentarios

---
+++ pagina/social-admin.js
@@ -0,0 +1,39 @@
+const publishBtn = document.getElementById("publish-btn");
+const postText = document.getElementById("post-text");
+const mainFeed = document.getElementById("main-feed");
+
+console.log("JS cargado correctamente");
+
+publishBtn.addEventListener("click", function () {
+
+    const textContent = postText.value.trim();
+
+    if (textContent === "") {
+        alert("Escribe algo antes de publicar");
+        return;
+    }
+
+    const newPost = document.createElement("article");
+
+    newPost.classList.add("post");
+
+    newPost.innerHTML = `
+        <div class="post-header">
+            <div class="avatar" style="background:#4CAF50;">
+                TU
+            </div>
+
+            <div>
+                <strong>Tu Nombre</strong>
+                <p>Administrador • Ahora mismo</p>
+            </div>
+        </div>
+
+        <p>${textContent}</p>
+    `;
+
+    mainFeed.prepend(newPost);
+
+    postText.value = "";
+
+});
